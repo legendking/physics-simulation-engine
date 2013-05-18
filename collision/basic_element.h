267e4b1983a82ab6607f177f486526ec1174379e
Huang Zen <Zen@HuangtekiMacBook-Pro.local>
2013-05-18 09:12:39 +0800

    a lot of problems fixed
    aha

--- collision/basic_element.h
+++ collision/basic_element.h
@@ -1,6 +1,3 @@
-#ifndef __BASICELEMENT_H
-#define __BASICELEMENT_H
-
 #include <cmath>
 
 //A defination of coordinate, a vector describe position, velosity or acceleration.
@@ -10,18 +7,18 @@ class coordinate{
   double x;
   double y;
   double z;
-
+    
   coordinate(){};
-  coordinate(double,double,double){};
+  coordinate(double,double,double);
   ~coordinate(){};
 
 
 //vector length
-  double length();
-  bool ifNULL();//double-type length may not be judged whether NULL, which may lead to zero-divide error
+  double length() const;
+  bool ifNULL() const;//double-type length may not be judged whether NULL, which may lead to zero-divide error
   //vector unit
   coordinate unit();
-}
+};
 
 
 //operator overloading
@@ -37,9 +34,11 @@ coordinate operator*(const double&,const coordinate&);
 coordinate operator/(const coordinate&,const double&);
 coordinate operator*(const coordinate&,const double&);
 //vector dot product
-double operator**(const coordinate&,const coordinate&);
+double dot(const coordinate&,const coordinate&);
+double operator*(const coordinate&,const coordinate&);
 //vector cross product
-coordinate operator*(const coordinate&,const coordinate&);
+coordinate cross(const coordinate&,const coordinate&);
+coordinate operator%(const coordinate&,const coordinate&);
 //vector parrallel
 bool parrallel(const coordinate&,const coordinate&);
 //vector vertical
@@ -53,11 +52,11 @@ class area{
   coordinate corner;//the corner point in this area with least x & y & z
   double x;//length by x-axis
   double y;//length by y-axis
-  double y;//length by z-axis
+  double z;//length by z-axis
  public:
   //determine whether a point is in this area
   bool contain(coordinate);
-}
+};
 
 // A defination of point, a basic object with position, vector and accleration.
 class point{
@@ -76,7 +75,7 @@ class point{
   void move(coordinate);
   //move point to certain position
   void moveto(coordinate);
-}
+};
 
 // A defination of mass point, a basic object in physics model
 class mass_point:public point
@@ -86,7 +85,7 @@ class mass_point:public point
  public:
   //return the force this mass point is applied
   coordinate ask_force();
-}
+};
 
 // A defination of point charge, a basic object in electromagnetic model
 class charge_point:public mass_point
@@ -95,18 +94,19 @@ class charge_point:public mass_point
   double charge;
  public:
   double ask_charge();
-}
+};
 
 // A defination of gravity field, a basic area which practise gravity force, as well as non-inertial force
 class gravity_field:public area
 {
  private:
   coordinate g;//Gravitation acceleration
-}
+};
+
 //A defination of electromagnetic field, a basic area which practise electromagnetic force
 class electromagnetic_field:public area
 {
  private:
   coordinate E;//electric field intensity
   coordinate B;//magnetic field intensity
-}
+};
